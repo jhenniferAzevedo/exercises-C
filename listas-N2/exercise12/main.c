5bb0dee6c84471ca8d9636d373b46ba6c248361a
Jhennifer Azevedo <143053335+jhenniferAzevedo@users.noreply.github.com>
2023-11-05 11:14:25 -0300

    just some changes

---
+++ listas-N2/exercise12/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#define MAX_X_Y 1000
+
+double distance(double point1, double point2);
+
+int main(void)
+{
+
+    double cartesian_point[MAX_X_Y][MAX_X_Y];
+
+    return 0;
+}
+
+/* Recebe dois pontos e retorna a distância entre eles */
+double distance(double point1, double point2)
+{
+}
---
+++ listas-N2/exercise12/readme.md
@@ -0,0 +1,3 @@
+# Exercício 12
+
+Escreva uma função que calcule e retorne a distância, em um plano cartesiano, entre dois pontos (x1, y1) e (x2, y2).
