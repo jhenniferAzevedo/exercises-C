efdd6f5be253d878297cd78b691aa90be3ba2ffe
jhenniferAzevedo <143053335+jhenniferAzevedo@users.noreply.github.com>
2023-11-05 11:38:50 -0300

    Update main.c (by cell phone)

--- provas-antigas/N1/2020-1-N1-Prova1/main.c
+++ provas-antigas/N1/2020-1-N1-Prova1/main.c
@@ -1,21 +1,11 @@
-/* Atividade Avaliativa N1 – 2020/1 17/11/2020 *1A*
-10 atletas foram monitorados por 8 semanas. Para cada atleta foram registrados sua idade, 
-várias medições do nível de oxigênio no sangue e medições semanais de frequência cardíaca. 
-Elabore um algoritmo que, para cada atleta, mostre seu nível médio de oxigênio no sangue e 
-a maior frequência cardíaca registrada durante o período de monitoramento. No final mostre 
-ainda quantos tiveram a média de oxigênio no sangue abaixo de 96 e a percentagem de atletas 
-com mais de 25 anos e a percentagem com menos de 16 anos. obs.: Na leitura das medições de 
-nível de oxigênio no sangue, utilize o valor -1 para indicar que a sequência de medições 
-finalizou (o valor -1 na entrada é o código para a parada da estrutura de repetição e não 
-deve ser processado). Considere que os valores de nível de oxigênio e frequência cardíaca 
-são valores reais. */
-
 #include <stdio.h>
+
+#define ATLETAS_MAX 10
+#define SEMANAS_MAX 8
+#define MAX 100
 
 int main(void)
 {
-    const int ATLETAS_MAX = 10, SEMANAS_MAX = 8, MAX = 100;
-
     int atleta, semana, nivOxigenio[MAX][ATLETAS_MAX], contOx,
         freqCardiaca[SEMANAS_MAX][ATLETAS_MAX], idade[ATLETAS_MAX], 
         maiorFreqCardiaca[ATLETAS_MAX], contOxMenor96 = 0,
