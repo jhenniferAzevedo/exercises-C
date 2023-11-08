d135ec141df09738ac86adfdfa39afceca1a492d
Jhennifer Azevedo <143053335+jhenniferAzevedo@users.noreply.github.com>
2023-11-07 22:30:18 -0300

    incomplete exercise added (2023.1 N2 prova)

--- provas-antigas/N2/2023-1-N2-Prova1/main.c
+++ provas-antigas/N2/2023-1-N2-Prova1/main.c
@@ -48,24 +48,8 @@ int main(void)
     return 0;
 }
 
-/*  Uma loja fará uma consulta de opinião com seus clientes por 5 dias. A cada dia no máximo
- *  20 clientes serão questionados (não receber o número de clientes como entrada):
- * 
- *  1. Encontrou o que procurava?
- *  2. Ficou satisfeito?
- * 
- *  Faça um programa em linguagem C que leia as respostas dos clientes e, após as leituras,
- *  mostre um destes relatórios:
- * 
- *  - Relatório por dia: dado o dia, mostrar todas as respostas deste dia. ✓
- *  - Relatório por pergunta: dada a pergunta, mostre a percentagem de respostas “sim” e a
- *    percentagem de respostas “não”. ✓
- *  - Relatório por cliente: dados o número do cliente e o dia, mostre as respostas; ✓
- *  - Relatório geral:
- *      + mostre quantos clientes foram consultados ✓
- *      + quantos responderam “não” para a duas questões ✓
- *      + quantos estão satisfeitos ✓
- *      + a percentagem de clientes que encontraram o que procuravam no 5° dia de pesquisa ✓
- *  Após imprimir um relatório, oferecer a opção para o usurário imprimir outro relatório se
- *  desejar.
+/*  Ainda falta: 
+ *  zerar as variáveis contadoras
+ *  conferir se ha algum erro nas funções
+ *  adicionar os relatorios finais
  */
