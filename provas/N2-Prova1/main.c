e6768ad8205dd90d37f381db63fbb8bb4e3152f0
Jhennifer Azevedo <143053335+jhenniferAzevedo@users.noreply.github.com>
2023-11-10 05:10:53 -0300

    update

--- provas/N2-Prova1/main.c
+++ provas/N2-Prova1/main.c
@@ -5,17 +5,7 @@
 
 int main(void)
 {
-    int n_veiculo = 0, resp[2];
-
-    int cadastro_veiculo[25][3],
-        cor = 0, codigo_veiculo = 1, qntd = 2;
-
-    int contador[5],
-        total_cadastros = 0,
-        total_veiculos = 1,
-        total_veiculos_por_cor = 2,
-        total_veiculos_por_codigo = 3,
-        total_veiculos_por_codigo_e_cor = 4;
+    int cadastro_veiculo[25][3], contador[5], resp;
     init_array(contador, 4);
     read_data(cadastro_veiculo, contador);
 
@@ -23,11 +13,10 @@ int main(void)
 
     do
     {
-        resp[0] = 0;
         menu_report();
-        scanf("%d", &resp[0]);
+        scanf("%d", &resp);
 
-        switch (resp[0])
+        switch (resp)
         {
         case 1:
             show_general_report(cadastro_veiculo, contador);
@@ -56,5 +45,5 @@ int main(void)
             printf("\nValor invalido! \nDigite um numero valo.\n");
             break;
         }
-    } while (resp[0] != 0);
+    } while (resp != 0);
 }
